1480646a50fb1f9d4f98d9769a375bbfebc4cc60
Nepcam <cameron.nepe@gmail.com>
2018-10-31 19:15:19 +1300

    updated js

--- calculator.js
+++ calculator.js
@@ -6,8 +6,8 @@
 
 
 const calculator = document.querySelector('.calculator')
-//const keys = calculator.querySelector('.calculator_keys')
-const display = document.querySelector('calculator_display')
+const keys = calculator.querySelector('.calculator__keys')
+const display = document.querySelector('calculator__display')
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
