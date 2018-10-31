a39f9c3923e2c438294f115c43f3d97facece7e8
Nepcam <cameron.nepe@gmail.com>
2018-10-31 18:25:52 +1300

    re-done js

--- calculator.js
+++ calculator.js
@@ -1,64 +1,36 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator__keys')
-const display = document.querySelector('calculator__display')
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
-        const key = e.target
-        const action = key.dataset.action
-        const keyContent = key.textContent
-        const displayedNum = display.textContent
         // Do something
-        Array.from(key.parentNode.children)
-            .forEach(k => k.classList.remove('is-depressed'))
     }
-    if ('action') {
-        console.log('number key!')
-    }
-    
-    if (
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-    ) {
-        console.log('operator key!')
-        key.classList.add('is-depressed')
-    }
-    
-    if (action === 'decimal') {
-        console.log('decimal key!')
-        display.textContent = displayedNum + '.'
-    }
-    
-    if (action === 'clear') {
-        console.log('clear key!')
-    }
-    
-    if (action === 'calculate') {
-        console.log('equal key!')
-    }
-    
-    if (!action) {
-        if (displayedNum === '0') {
-            display.textContent = keyContent
-        }
-    }
-    
-    if (!action) {
-        if (displayedNum === '0') {
-            display.textContent = keyContent
-        } 
-        else {
-            display.textContent = displayedNum + keyContent
-        }
-    }
-    
 })
-
-
 
 const key = e.target
 const action = key.dataset.action
 
+if (!action) {
+    console.log('number key!')
+}
 
+if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+) {
+    console.log('operator key!')
+}
+
+if (action === 'decimal') {
+    console.log('decimal key!')
+  }
+  if (action === 'clear') {
+    console.log('clear key!')
+  }
+  if (action === 'calculate') {
+    console.log('equal key!')
+  }
+
+  
