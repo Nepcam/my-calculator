251b16fd69c58ac39a201960429f4b6286014c6e
Nepcam <cameron.nepe@gmail.com>
2019-01-04 22:58:01 +1300

    working on the calculator

---
+++ calc.js
@@ -0,0 +1,41 @@
+
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+
+
+// listening for key presses 
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        // Do something
+    }
+})
+
+const key = e.target
+const action = key.dataset.action
+
+//if key does not have a data-action attribute, it must be a number key
+if (!action) {
+    console.log('number key!')
+}
+
+// if key has a data-action + - * or / we know that the key is an operator
+if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+) {
+    console.log('operator key!')
+}
+
+// if key has a data-action of . clear or = 
+if (action === 'decimal!') {
+    console.log('decimal key')
+}
+if(action === 'clear') {
+    console.log('clear key!')
+}
+if (action === 'calculate') {
+    console.log('equal key!')
+}
+
--- calculator.js
+++ calculator.js
@@ -1,77 +1,78 @@
-// Checklist
-// 1. user selects number, display the number 
-// 2. then user select the operator
-// 3. clear screen and display the selected number, and calculate
-// 4. display the answer
+// // Checklist
+// // 1. user selects number, display the number 
+// // 2. then user select the operator
+// // 3. clear screen and display the selected number, and calculate
+// // 4. display the answer
 
 
-const calculator = document.querySelector('.calculator')
-const keys = document.querySelector('.calculator__keys')
-const display = document.querySelector('calculator__display')
+// const calculator = document.querySelector('.calculator')
+// const keys = document.querySelector('.calculator__keys')
+// const display = document.querySelector('calculator__display')
+// const key = e.target
+// const action = key.dataset.action
+// const keyContent = key.textContent
+// const displayedNum = display.textContent
 
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-        const key = e.target
-        const action = key.dataset.action
-        const keyContent = key.textContent
-        const displayedNum = display.textContent
-        // // Do something
-        // Array.from(key.parentNode.children)
-        //     .forEach(k => k.classList.remove('is-depressed'))
-    }
-    if ('action') {
-        console.log('number key!')
-    }
+// keys.addEventListener('click', e => {
+//     if (e.target.matches('button')) {
+       
+//         // // Do something
+//         // Array.from(key.parentNode.children)
+//         //     .forEach(k => k.classList.remove('is-depressed'))
+//     }
+//     if ('action') {
+//         console.log('number key!')
+//     }
     
-    if (
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-    ) {
-        console.log('operator key!')
-        key.classList.add('is-depressed')
-    }
+//     if (
+//         action === 'add' ||
+//         action === 'subtract' ||
+//         action === 'multiply' ||
+//         action === 'divide'
+//     ) {
+//         console.log('operator key!')
+//         key.classList.add('is-depressed')
+//     }
     
-    if (action === 'decimal') {
-        console.log('decimal key!')
-        display.textContent = displayedNum + '.'
-    }
+//     if (action === 'decimal') {
+//         console.log('decimal key!')
+//         display.textContent = displayedNum + '.'
+//     }
     
-    if (action === 'clear') {
-        console.log('clear key!')
-    }
+//     if (action === 'clear') {
+//         console.log('clear key!')
+//     }
     
-    if (action === 'calculate') {
-        let firstValue = calculator.dataset.firstValue
-        const operator = calculator.dataset.operator
-        const secondValue = displayedNum
-        console.log('equal key!')
-    }
+//     if (action === 'calculate') {
+//         let firstValue = calculator.dataset.firstValue
+//         const operator = calculator.dataset.operator
+//         const secondValue = displayedNum
+//         console.log('equal key!')
+//     }
     
-    if (!action) {
-        if (displayedNum === '0') {
-            display.textContent = keyContent
-        }
-    }
+//     if (!action) {
+//         if (displayedNum === '0') {
+//             display.textContent = keyContent
+//         }
+//     }
     
-    if (!action) {
-        if (displayedNum === '0') {
-            display.textContent = keyContent
-        } 
-        else {
-            display.textContent = displayedNum + keyContent
-        }
-    } 
+//     if (!action) {
+//         if (displayedNum === '0') {
+//             display.textContent = keyContent
+//         } 
+//         else {
+//             display.textContent = displayedNum + keyContent
+//         }
+//     } 
     
-    if (firstValue) {
-        if (previousKeyType === 'calculate') {
-            firstValue = displayedNum
-        }
-    display.textContent = calculate(firstValue, operator, secondValue)
-    }
-    calculator.dataset.previousKeyType = 'calculate'
-})
+//     if (firstValue) {
+//         if (previousKeyType === 'calculate') {
+//             firstValue = displayedNum
+//         }
+//     display.textContent = calculate(firstValue, operator, secondValue)
+//     }
+//     calculator.dataset.previousKeyType = 'calculate'
+// })
 
 
 
