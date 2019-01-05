18fe0c288cbb0763aa1bed4d7fcaa46e9d2f9867
Nepcam <cameron.nepe@gmail.com>
2019-01-05 22:04:41 +1300

    working on event listener (key presses)

--- calc.js
+++ calc.js
@@ -2,22 +2,19 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator__keys')
 
-
 // listening for key presses 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         // Do something
+        const key = e.target
+        const action = key.dataset.action
+        // if key does not have a data-action attribute, it must be a number key
+        if (!action) {
+        console.log('number key!')
+        }
     }
     
 })
-
-const key = e.target
-const action = key.dataset.action
-
-//if key does not have a data-action attribute, it must be a number key
-if (!action) {
-    console.log('number key!')
-}
 
 // if key has a data-action + - * or / we know that the key is an operator
 if (
